505edd6ec19e1d787b76d4d6d965392db2e2fe44
Urmit Kaundal <urmitkaundal53@gmail.com>
2025-08-08 01:39:14 +0530

    Added add.js file

---
+++ add.js
@@ -0,0 +1,9 @@
+// Step 1: Define two numbers
+let num1 = 5;
+let num2 = 8;
+
+// Step 2: Perform addition
+let sum = num1 + num2;
+
+// Step 3: Display the result
+console.log("The sum is: " + sum);
